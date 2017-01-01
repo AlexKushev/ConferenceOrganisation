0600d186d61991a88528819191f96791321ffeb7
Alex Kushev <alex.kushev.bg@gmail.com>
2017-01-01 18:37:09 +0200

    remove unused code

--- src/main/java/conferenceOrganisation/database/connection/DatabaseConnection.java
+++ src/main/java/conferenceOrganisation/database/connection/DatabaseConnection.java
@@ -48,40 +48,4 @@ public class DatabaseConnection {
 
 		return connection.createStatement();
 	}
-	
-	public Connection createConnection() throws IOException {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			System.out.println("No driver!");
-		}
-
-		Connection connection = null;
-
-		try {
-			Properties prop = new Properties();
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			InputStream input = classLoader
-					.getResourceAsStream("conferenceOrganisation/database/connection/config.properties");
-
-			prop.load(input);
-
-			String databaseUrl = String.format("jdbc:mysql://%s:%s/%s", prop.getProperty("databaseNetworkAddress"),
-					prop.getProperty("databasePort"), prop.getProperty("databaseName"));
-
-			connection = DriverManager.getConnection(databaseUrl, prop.getProperty("databaseUserAccount"),
-					prop.getProperty("databasePassword"));
-		} catch (SQLException e) {
-			System.out.println("Connection failed");
-		}
-
-		if (connection != null) {
-			System.out.println("Connected successfuly to the database.");
-		} else {
-			System.out.println("Connection failed.");
-		}
-		
-		return connection;
-	}
-
 }
