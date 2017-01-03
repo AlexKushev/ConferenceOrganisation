a1a1c706d9f7aae53d430c508cc2cc5b335fae6a
MarinaGeorgieva <marina.iv.georgieva@gmail.com>
2017-01-03 14:32:33 +0200

    Implemented upcoming events filtering by city

--- src/main/webapp/js/upcomingEvents.js
+++ src/main/webapp/js/upcomingEvents.js
@@ -1,16 +1,41 @@
 $(document).ready(function() {
 
+  	var selectedOption= $('#filter-by-city').val();
+
+  	if (selectedOption === 'All') {
+  		getAllEvents();
+  	}
+  	else {
+  		filterByCity(selectedOption);
+  	}
+
+  	$('#filter-by-city').change(function() {
+  		selectedOption = $('#filter-by-city').val();
+
+  		if (selectedOption ==='All') {
+  			getAllEvents();
+  		}
+  		else {
+  			filterByCity(selectedOption);
+  		}
+  	});
+});
+
+function createEventsHtml(eventsData) {
+	$('#events-container').empty();
+	$('#events-container article').hide();
+
 	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
   						"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   	var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-	$.getJSON('rest/events', function(response) {
-		var eventsData = response.event;
+	var i, len = eventsData.length;
 
-		console.log(eventsData);
-
-		var i, len = eventsData.length;
+	if (len === 0) {
+		$('#events-container').text('No events :(');
+	}
+	else {
 		for (i = 0; i < len; i++) {
 			var id = eventsData[i].eventId;
 			var title = eventsData[i].title;
@@ -53,27 +78,52 @@ $(document).ready(function() {
             	'</article>';
 
             	$('#events-container').append(eventHtml);
-			}
+			}	
 		}
+	}	
+}
+
+function filterByCity(city) {
+	$.getJSON('rest/events/eventsByCity?city=' + city, function(response) {
+		var eventsData = response.event;
+
+		createEventsHtml(eventsData);
+
 	}).done(function() {
-		var loadMoreButton = $('#load-more-button');
-
-		var articlesSize = $("#events-container article").size();
-    	var itemsToShow = 10;
-
-    	$('#events-container article:lt(' + itemsToShow + ')').show();
-
-    	if (itemsToShow >= articlesSize) {
-        		loadMoreButton.hide();
-        	}
-
-    	loadMoreButton.click(function () {
-        	itemsToShow = (itemsToShow + 10 <= articlesSize) ? itemsToShow + 10 : articlesSize;
-        	$('#events-container article:lt(' + itemsToShow + ')').show();
-
-        	if (itemsToShow == articlesSize) {
-        		loadMoreButton.hide();
-        	}
-    	});
+		loadMore();
 	});
-});
+}
+
+function getAllEvents() {
+	$.getJSON('rest/events', function(response) {
+		var eventsData = response.event;
+
+		createEventsHtml(eventsData);
+
+	}).done(function() {
+		loadMore();
+	});
+}
+
+function loadMore() {
+	var loadMoreButton = $('#load-more-button');
+	loadMoreButton.show();
+
+	var articlesSize = $("#events-container article").size();
+    var itemsToShow = 10;
+
+    $('#events-container article:lt(' + itemsToShow + ')').show();
+
+    if (itemsToShow >= articlesSize) {
+        loadMoreButton.hide();
+    }
+
+    loadMoreButton.click(function () {
+        itemsToShow = (itemsToShow + 10 <= articlesSize) ? itemsToShow + 10 : articlesSize;
+        $('#events-container article:lt(' + itemsToShow + ')').show();
+
+        if (itemsToShow == articlesSize) {
+        	loadMoreButton.hide();
+        }
+    });
+}
