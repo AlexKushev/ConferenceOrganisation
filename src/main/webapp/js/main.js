b62eafcffe92b870cfea9f50bf17d3462348b3b5
Sevdalin Zhelyazkov <irrealizable@abv.bg>
2017-01-06 19:27:44 +0200

    Removed modal form input box validation message

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,47 +1,47 @@
  $(document).ready(function() {
 
-    var navigation = '<div class="topline clear">' +
-	    '<div class="topline__left">' +
-	        '<nav id="desktop-menu" class="desktopMenu clear">' +
-	            '<a href="index.html">Home</a>' +
-	        '</nav>' +
-	        '<nav class="mobileMenu">' +
-	            '<a href="#" class="mobileMenu__btn"></a>' +
-	            '<div id="mobile-menu" class="mobileMenu__items">' +
-	                '<a href="index.html">Home</a>' +
-	           '</div>' +
-	        '</nav>' +
-	    '</div>' +
-	    '<div class="topline__right profileMenu">' +
-	        '<a id="account" href="#" class="profileMenu__btn profileMenu__btn--account" role="button" data-toggle="modal" data-target="#login-modal">Account</a>' +
-	        '<button id="logout-button" type="button" class="profileMenu__btn profileMenu__btn--logout">Logout</button>' +
-	    '</div>' +
-	'</div>';
+     var navigation = '<div class="topline clear">' +
+         '<div class="topline__left">' +
+         '<nav id="desktop-menu" class="desktopMenu clear">' +
+         '<a href="index.html">Home</a>' +
+         '</nav>' +
+         '<nav class="mobileMenu">' +
+         '<a href="#" class="mobileMenu__btn"></a>' +
+         '<div id="mobile-menu" class="mobileMenu__items">' +
+         '<a href="index.html">Home</a>' +
+         '</div>' +
+         '</nav>' +
+         '</div>' +
+         '<div class="topline__right profileMenu">' +
+         '<a id="account" href="#" class="profileMenu__btn profileMenu__btn--account" role="button" data-toggle="modal" data-target="#login-modal">Account</a>' +
+         '<button id="logout-button" type="button" class="profileMenu__btn profileMenu__btn--logout">Logout</button>' +
+         '</div>' +
+         '</div>';
 
-	$('.toplineWrap').append(navigation);
+     $('.toplineWrap').append(navigation);
 
-    $('.profileMenu__btn--logout').hide();
-    $('#account').attr("data-toggle", "modal");
-    $('#account').attr("data-target", "#login-modal");
-    $('#account').text('Account');
+     $('.profileMenu__btn--logout').hide();
+     $('#account').attr("data-toggle", "modal");
+     $('#account').attr("data-target", "#login-modal");
+     $('#account').text('Account');
 
-    authUser();
+     authUser();
 
-    var registerButton = $('#register-button'),
-        loginButton = $('#login-button'),
-        logoutButton = $('#logout-button');
+     var registerButton = $('#register-button'),
+         loginButton = $('#login-button'),
+         logoutButton = $('#logout-button');
 
-    registerButton.on('click', function() {
-        register();
-    });
+     registerButton.on('click', function() {
+         register();
+     });
 
-    loginButton.on('click', function() {
-        login();
-    });
+     loginButton.on('click', function() {
+         login();
+     });
 
-    logoutButton.on('click', function(e) {
-        logout();
-    });
+     logoutButton.on('click', function(e) {
+         logout();
+     });
 
      $(window).scroll(function() {
          if ($(this).scrollTop() > 350) {
@@ -61,139 +61,138 @@
  });
 
  function login() {
-    var userEmail = $('#login_email').val(),
-        userPassword = $('#login_password').val();
+     var userEmail = $('#login_email').val(),
+         userPassword = $('#login_password').val();
 
 
-    var userData = {
-        user: {
-            email: userEmail,
-            password: userPassword
-        }
-    };
+     var userData = {
+         user: {
+             email: userEmail,
+             password: userPassword
+         }
+     };
 
-    $.ajax({
-        type: 'POST',
-        url: 'rest/user/login',
-        contentType: 'application/json',
-        data: JSON.stringify(userData)
-    }).done(function() {
-        // destroy modal login 
-        $('#login-modal').modal('toggle');
+     $.ajax({
+         type: 'POST',
+         url: 'rest/user/login',
+         contentType: 'application/json',
+         data: JSON.stringify(userData)
+     }).done(function() {
+         // destroy modal login 
+         $('#login-modal').modal('toggle');
 
-        // set current user
-        authUser();
+         // set current user
+         authUser();
 
-        alert("Successfully logged in!");
-        location.reload();
+         alert("Successfully logged in!");
+         location.reload();
 
-    }).fail(function() {
-        alert("Wrong e-mail or password!");
-    }).always(function() {
-            
-    });
+     }).fail(function() {
+         alert("Wrong e-mail or password!");
+     }).always(function() {
+
+     });
  }
 
  function register() {
-    var userEmail = $('#register_email').val(),
-        userPassword = $('#register_password').val(),
-        userFirstName = $('#register_firstName').val(),
-        userLastName = $('#register_lastName').val();
+     var userEmail = $('#register_email').val(),
+         userPassword = $('#register_password').val(),
+         userFirstName = $('#register_firstName').val(),
+         userLastName = $('#register_lastName').val();
 
-    var userData = {
-        user: {
-            email: userEmail,
-            password: userPassword,
-            firstName: userFirstName,
-            lastName: userLastName
-        }
-    };
+     var userData = {
+         user: {
+             email: userEmail,
+             password: userPassword,
+             firstName: userFirstName,
+             lastName: userLastName
+         }
+     };
 
-    if (!validateUserData(userEmail, userPassword, userFirstName, userLastName)) {
-        alert('Invalid data!');
-        return;
-    }
+     if (!validateUserData(userEmail, userPassword, userFirstName, userLastName)) {
+         alert('Invalid data!');
+         return;
+     }
 
-    $.ajax({
-        type: 'POST',
-        url: 'rest/user/register',
-        contentType: 'application/json',
-        data: JSON.stringify(userData)
-    }).done(function() {
-        alert('Successfully registered!');
-    }).fail(function() {
-        alert('Invalid data or user with this e-mail already exists!');
-    }).always(function() {
-        // submit
-    });
+     $.ajax({
+         type: 'POST',
+         url: 'rest/user/register',
+         contentType: 'application/json',
+         data: JSON.stringify(userData)
+     }).done(function() {
+         alert('Successfully registered!');
+     }).fail(function() {
+         alert('Invalid data or user with this e-mail already exists!');
+     }).always(function() {
+         // submit
+     });
  }
 
  function validateUserData(userEmail, userPassword, userFirstName, userLastName) {
-    function validateLength(str, min, max) {
-        return str.length >= min && str.length <= max;
-    }
+     function validateLength(str, min, max) {
+         return str.length >= min && str.length <= max;
+     }
 
-    function validateIfEmpty(str) {
-        if (str.trim() === null || str.trim() === '' || str.trim() === ' ') {
-            return true;
-        }
+     function validateIfEmpty(str) {
+         if (str.trim() === null || str.trim() === '' || str.trim() === ' ') {
+             return true;
+         }
 
-        return false;
-    }
+         return false;
+     }
 
-    function validateEmail(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    }
+     function validateEmail(email) {
+         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+         return re.test(email);
+     }
 
-    if (!validateEmail(userEmail) || validateIfEmpty(userPassword) || !validateLength(userPassword, 5, 20) || validateIfEmpty(userFirstName) || !validateLength(userFirstName, 2, 20) || validateIfEmpty(userLastName) || !validateLength(userLastName, 2, 20)) {
-        return false;
-    }
+     if (!validateEmail(userEmail) || validateIfEmpty(userPassword) || !validateLength(userPassword, 5, 20) || validateIfEmpty(userFirstName) || !validateLength(userFirstName, 2, 20) || validateIfEmpty(userLastName) || !validateLength(userLastName, 2, 20)) {
+         return false;
+     }
 
-    return true;
+     return true;
  }
 
  function authUser() {
-    $.getJSON('rest/user/current', function(response) {
-        if (!response) {
-            $('.profileMenu__btn--logout').hide();
-            $('#not-logged-in-header').addClass('active');
-            return;
-        }
-        else {
-            var currentUser = response.user;
+     $.getJSON('rest/user/current', function(response) {
+         if (!response) {
+             $('.profileMenu__btn--logout').hide();
+             $('#not-logged-in-header').addClass('active');
+             return;
+         } else {
+             var currentUser = response.user;
 
-            $('#filter-line-tabs').append('<a href="addevent.html">Add New</a>');
-            var currentPage = document.location.pathname.match(/[^\/]+$/)[0];
-            if (currentPage.indexOf('addevent') != -1) {
-                $('#filter-line-tabs a:last-child').addClass('active');
-            }
+             $('#filter-line-tabs').append('<a href="addevent.html">Add New</a>');
+             var currentPage = document.location.pathname.match(/[^\/]+$/)[0];
+             if (currentPage.indexOf('addevent') != -1) {
+                 $('#filter-line-tabs a:last-child').addClass('active');
+             }
 
-            $('#logged-in-header').addClass('active');
-            $('.profileMenu__btn--logout').show();
-            $('#account').removeAttr("data-toggle");
-            $('#account').removeAttr("data-target");
-            $('#account').text(currentUser.firstName);
+             $('#logged-in-header').addClass('active');
+             $('.profileMenu__btn--logout').show();
+             $('#account').removeAttr("data-toggle");
+             $('#account').removeAttr("data-target");
+             $('#account').text(currentUser.firstName);
 
-            $('#desktop-menu').append(' <a href="eventmanager.html">Event Manager</a>');
-            $('#mobile-menu').append(' <a href="eventmanager.html">Event Manager</a>');
+             $('#desktop-menu').append(' <a href="eventmanager.html">Event Manager</a>');
+             $('#mobile-menu').append(' <a href="eventmanager.html">Event Manager</a>');
 
-            // if user is admin add this -> <a href="adminpanel.html">Admin Panel</a>
-        }       
-    });
+             // if user is admin add this -> <a href="adminpanel.html">Admin Panel</a>
+         }
+     });
  }
 
  function logout() {
-    $.ajax({
-        type: 'GET',
-        url: 'rest/user/logout',
-        statusCode: {
-            204: function() {
-                alert("Successfully logged out!");
-                window.location.replace('index.html');
-            }
-        }
-    });
+     $.ajax({
+         type: 'GET',
+         url: 'rest/user/logout',
+         statusCode: {
+             204: function() {
+                 alert("Successfully logged out!");
+                 window.location.replace('index.html');
+             }
+         }
+     });
  }
 
  // Mobile Menu Toggle
@@ -201,97 +200,4 @@
      var menu = $('.mobileMenu__items');
      menu.toggleClass('active');
      $(this).toggleClass('active');
- });
-
- /* #####################################################################
-    #
-    #   Project       : Modal Login with jQuery Effects
-    #   Author        : Rodrigo Amarante (rodrigockamarante)
-    #   Version       : 1.0
-    #   Created       : 07/29/2015
-    #   Last Change   : 08/04/2015
-    #
-    ##################################################################### */
-
- $(function() {
-
-     var $formLogin = $('#login-form');
-     var $formLost = $('#lost-form');
-     var $formRegister = $('#register-form');
-     var $divForms = $('#div-forms');
-     var $modalAnimateTime = 300;
-     var $msgAnimateTime = 150;
-     var $msgShowTime = 2000;
-
-     $("form").submit(function() {
-         switch (this.id) {
-             case "login-form":
-                 var $lg_email = $('#login_email').val();
-                 var $lg_password = $('#login_password').val();
-                 if ($lg_email == "ERROR") {
-                     msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Login error");
-                 } else {
-                     msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Login OK");
-                 }
-                 return false;
-             case "lost-form":
-                 var $ls_email = $('#lost_email').val();
-                 if ($ls_email == "ERROR") {
-                     msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", "Send error");
-                 } else {
-                     msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "success", "glyphicon-ok", "Send OK");
-                 }
-                 return false;
-             case "register-form":
-                 var $rg_email = $('#register_email').val();
-                 var $rg_password = $('#register_password').val();
-                 if ($rg_email == "ERROR") {
-                     msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Register error");
-                 } else {
-                     msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "success", "glyphicon-ok", "Register OK");
-                 }
-                 return false;
-             default:
-                 return false;
-         }
-         return false;
-     });
-
-     $('#login_register_btn').click(function() { modalAnimate($formLogin, $formRegister); });
-     $('#register_login_btn').click(function() { modalAnimate($formRegister, $formLogin); });
-     $('#login_lost_btn').click(function() { modalAnimate($formLogin, $formLost); });
-     $('#lost_login_btn').click(function() { modalAnimate($formLost, $formLogin); });
-     $('#lost_register_btn').click(function() { modalAnimate($formLost, $formRegister); });
-     $('#register_lost_btn').click(function() { modalAnimate($formRegister, $formLost); });
-
-     function modalAnimate($oldForm, $newForm) {
-         var $oldH = $oldForm.height();
-         var $newH = $newForm.height();
-         $divForms.css("height", $oldH);
-         $oldForm.fadeToggle($modalAnimateTime, function() {
-             $divForms.animate({ height: $newH }, $modalAnimateTime, function() {
-                 $newForm.fadeToggle($modalAnimateTime);
-             });
-         });
-     }
-
-     function msgFade($msgId, $msgText) {
-         $msgId.fadeOut($msgAnimateTime, function() {
-             $(this).text($msgText).fadeIn($msgAnimateTime);
-         });
-     }
-
-     function msgChange($divTag, $iconTag, $textTag, $divClass, $iconClass, $msgText) {
-         var $msgOld = $divTag.text();
-         msgFade($textTag, $msgText);
-         $divTag.addClass($divClass);
-         $iconTag.removeClass("glyphicon-chevron-right");
-         $iconTag.addClass($iconClass + " " + $divClass);
-         setTimeout(function() {
-             msgFade($textTag, $msgOld);
-             $divTag.removeClass($divClass);
-             $iconTag.addClass("glyphicon-chevron-right");
-             $iconTag.removeClass($iconClass + " " + $divClass);
-         }, $msgShowTime);
-     }
  });
